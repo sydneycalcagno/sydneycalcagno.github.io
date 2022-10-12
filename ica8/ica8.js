ec46a1398aba5fd10c4c52e24bc48c5df4125d8f
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-10-11 19:42:40 -0600

    yuh

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -1,6 +1,22 @@
 let body = document.querySelector('body');
-body.addEventListener('click', giveAlert());
+body.getElementById("body").addEventListener('click', giveAlert());
 
 function giveAlert(){
     alert('this is an alert');
+}
+
+let hov = document.querySelector('hov');
+hov.addEventListener('hover', color);
+
+function color(){
+
+}
+
+var color = ["#eca1a6","#b5e7a0", "#92a8d1", "#ffef96"];
+var i = 0;
+document.querySelector("button").addEventListener("click", col())
+
+function col(){
+    i = i < color.length ? ++i : 0;
+    document.querySelector("body").style.background = color[i]
 }
