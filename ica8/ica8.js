7c9668394e810914e02b57869fcf788450ba7e67
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-10-11 20:23:14 -0600

    TWO

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -26,3 +26,9 @@ const bn = document.getElementById('bn');
 bn.addEventListener('click', function click(event){
     document.body.style.backgroundColor = 'pink';
 });
+
+const hov = document.getElementById('hov');
+hov.addEventListener('mouseover', function h(event){
+    document.body.style.backgroundColor = 'aqua';
+});
+
