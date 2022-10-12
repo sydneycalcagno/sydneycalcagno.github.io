dec318532246c620d7347c7c3f144f9f1bec6537
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-10-11 20:15:23 -0600

    ONE DOWN YAY

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -1,22 +1,28 @@
-let body = document.querySelector('body');
-body.getElementById("body").addEventListener('click', giveAlert());
+// let body = document.querySelector('body');
+// body.getElementById("body").addEventListener('click', giveAlert());
 
-function giveAlert(){
-    alert('this is an alert');
-}
+// function giveAlert(){
+//     alert('this is an alert');
+// }
 
-let hov = document.querySelector('hov');
-hov.addEventListener('hover', color);
+// let hov = document.querySelector('hov');
+// hov.addEventListener('hover', color);
 
-function color(){
+// function color(){
 
-}
+// }
 
-var color = ["#eca1a6","#b5e7a0", "#92a8d1", "#ffef96"];
-var i = 0;
-document.querySelector("button").addEventListener("click", col())
+// var color = ["#eca1a6","#b5e7a0", "#92a8d1", "#ffef96"];
+// var i = 0;
+// document.querySelector("button").addEventListener("click", col())
 
-function col(){
-    i = i < color.length ? ++i : 0;
-    document.querySelector("body").style.background = color[i]
-}
+// function col(){
+//     for (let i = 0; i < cars.length; i++){
+//         document.querySelector("body").style.background = color[i]
+//     }
+// }
+
+const bn = document.getElementById('bn');
+bn.addEventListener('click', function click(event){
+    document.body.style.backgroundColor = 'pink';
+});
