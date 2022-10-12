fffb44eda7b7b6c889077eb96984fd7b68567c23
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-10-11 20:28:25 -0600

    whoop

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -1,27 +1,3 @@
-// let body = document.querySelector('body');
-// body.getElementById("body").addEventListener('click', giveAlert());
-
-// function giveAlert(){
-//     alert('this is an alert');
-// }
-
-// let hov = document.querySelector('hov');
-// hov.addEventListener('hover', color);
-
-// function color(){
-
-// }
-
-// var color = ["#eca1a6","#b5e7a0", "#92a8d1", "#ffef96"];
-// var i = 0;
-// document.querySelector("button").addEventListener("click", col())
-
-// function col(){
-//     for (let i = 0; i < cars.length; i++){
-//         document.querySelector("body").style.background = color[i]
-//     }
-// }
-
 const bn = document.getElementById('bn');
 bn.addEventListener('click', function click(event){
     document.body.style.backgroundColor = 'pink';
@@ -29,6 +5,6 @@ bn.addEventListener('click', function click(event){
 
 const hov = document.getElementById('hov');
 hov.addEventListener('mouseover', function h(event){
-    document.body.style.backgroundColor = 'aqua';
+    hov.style.color = 'aqua';
 });
 
