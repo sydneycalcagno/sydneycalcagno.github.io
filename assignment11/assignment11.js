c7dcd61cb5f5d3f56805adfb795eebdc9526e0f1
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-11-05 17:17:31 -0600

    finnallyy

--- assignment11/assignment11.js
+++ assignment11/assignment11.js
@@ -1,28 +1,46 @@
-const quote = document.querySelector('.new-quote'); 
-quote.addEventListener('click', newInspire); 
-window.addEventListener('load', newInspire);
 
-const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', nQuote);
+window.addEventListener('load', nQuote);
 
-function display(inspire){
-    const text = document.querySelector('#js-quote-text');
-    text.textContent = inspire; 
+const endpoint = 'https://catfact.ninja/fact';
+
+
+
+function displayQuote(newFact){
+    const txt = document.querySelector('#js-quote-text'); 
+    txt.textContent = newFact;
 }
 
+async function nQuote(){
 
-async function newInspire(endpoint) {
-    try{
-        const resposne = await fetch(endpoint);
-
-        if (!response.ok) { 
-            throw Error(response.statusText);
-        }
-
-        const json = await response.json(); 
-        display(json.message);
-    }
-    catch (err) { //if try doesnt work
-        console.log(err);
-        alert('Failed, check connection');
-    }
+    let response = await fetch(endpoint);
+    let data = await response.json(); 
+    
+    newFact = data.fact;
+    displayQuote(newFact);
 }
+
+const bn = document.getElementById('js-tweet');
+bn.addEventListener('click', function click(event){
+    let rnd = Math.floor(Math.random() * 7);
+    if(rnd == 1){
+        document.body.style.backgroundColor = 'red';
+    }
+    if(rnd == 2){
+        document.body.style.backgroundColor = 'orange';
+    }
+    if(rnd == 3){
+        document.body.style.backgroundColor = 'yellow';
+    }
+    if(rnd == 4){
+        document.body.style.backgroundColor = 'green';
+    }
+    if(rnd == 5){
+        document.body.style.backgroundColor = 'blue';
+    }
+    if(rnd == 6){
+        document.body.style.backgroundColor = 'pink';
+    }
+});
+
