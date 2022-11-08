d86ff19516799422c02347bd9dcd02559bdd5a88
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2022-11-08 10:38:30 -0700

    oop

--- wa12/wa12.js
+++ wa12/wa12.js
@@ -1,9 +1,78 @@
-const button = document.querySelector('.button');
+// const button = document.querySelector('.button');
+// const output = document.querySelector('.output');
+// let phone_content = document.querySelector('.phone');
+
+// button.addEventListener('click', updateOutput);
+
+// function updateOutput() {
+//     output.textContent = phone_content.value;
+//     alert(phone_content.value); 
+// }
+
 const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
 
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
+function check(){
+    console.log('hi');
 }
+
+function submit(){
+    alert(output.textContent);
+}
+
+function reset(){
+   outputInt = 0; 
+   output.textContent = outputInt;
+}
+
+function plusRunner(){
+    plus(outputInt);
+}
+
+function plus(){
+    if(outputInt < 9999999999){
+        outputInt += 1;
+        output.textContent = outputInt;
+    }
+    
+}
+
+function minus(){
+    if(outputInt > 0){
+        outputInt -= 1;
+        output.textContent = outputInt;
+    }
+    
+}
+
+function random(){
+    outputInt = randomNum(0, 9999999999);
+    output.textContent = outputInt;
+}
+
+function randomNum(min, max){
+    const num = Math.floor(Math.random) * (max - min + 1) + min;
+    return num;
+}
+
+function update(){
+    outputSlide.textContent = slider.value;
+}
+
+
+var slider = document.getElementById("myRange");
+var sliderSubmit = document.querySelector(".slider").addEventListener('click', update);
+var outputSlide = document.getElementById("demo");
+outputSlide.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  outputSlide.innerHTML = this.value;
+}
+
+const minusButton = document.querySelector('.minusbutton').addEventListener('click', minus);
+const addButton = document.querySelector('.addbutton').addEventListener('click', plusRunner);
+const submitButton = document.querySelector('.submitbutton').addEventListener('click', submit);
+const resetButton = document.querySelector('.resetbutton').addEventListener('click', reset);
+const randomButton = document.querySelector('.randombutton').addEventListener('click', random);
